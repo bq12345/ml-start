2983129838870f53ea8ee692a16978e8fa1a35fa
BaiQiang <bq12345@users.noreply.github.com>
2017-12-22 16:17:04 +0800

    Update mnist_softmax.py

--- mnist/mnist_softmax.py
+++ mnist/mnist_softmax.py
@@ -67,7 +67,6 @@ def main(_):
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), y_)
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-  print("正确率：")
   print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
